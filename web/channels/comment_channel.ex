2b11dbb802a84bd3724b8cffadc143dddc924ac7
lizhe <lizhe19851001@126.com>
2016-04-07 16:00:48 +0800

    m

--- web/channels/comment_channel.ex
+++ web/channels/comment_channel.ex
@@ -11,6 +11,12 @@ defmodule Pxblog.CommentChannel do
     end
   end
 
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (comments:lobby).
   def handle_in("CREATED_COMMENT", payload, socket) do
